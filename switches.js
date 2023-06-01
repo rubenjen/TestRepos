c8c32da9f7dddea7b84c9d2d4b25940d78134c13
rubenjen <42122911+rubenjen@users.noreply.github.com>
2023-06-01 15:03:43 -0400

    Add files via upload

---
+++ switches.js
@@ -0,0 +1,15 @@
+var grade = "a";
+//grade = grade.toUpperCase();
+
+switch(grade){
+	case "A":
+		console.log("Not Bad");
+		break;
+	case "C":
+		console.log("Pass");
+		break;
+	default:{
+		console.log("What's That!");
+	}
+
+}
